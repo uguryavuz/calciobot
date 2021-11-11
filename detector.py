2fa193cc39d48b5db4865d1e077e3863ceafb720
uguryavuz <35351115+uguryavuz@users.noreply.github.com>
2021-11-11 06:19:40 +0000

    Some detection work.

--- detector.py
+++ detector.py
@@ -24,8 +24,11 @@ GRID_TOPIC = 'map'
 # Loop frequency (in 1/s i.e. Hertz; for msg sending main loop.)
 FREQUENCY = 30
 
-# OpenCV-ROS image converter instance
-bridge = CvBridge()
+# OpenCV things?
+# For color ranges: https://stackoverflow.com/questions/36817133/identifying-the-range-of-a-color-in-hsv-using-opencv/51686953
+# OpenCV uses H: 0 to 179, S: 0 to 255, V: 0 to 255
+blue_mask = lambda hsv_img: cv2.inRange(hsv_img, (90, 50, 70), (128, 255, 255))
+orange_mask = lambda hsv_img: cv2.inRange(hsv_img, (10, 50, 70), (35, 255, 255))
 
 class Detector():
     def __init__(self, freq=FREQUENCY):
@@ -41,6 +44,14 @@ class Detector():
         # Message transmission rate
         self._freq = freq
         self._rate = rospy.Rate(freq)
+
+        # OpenCV-ROS image converter instance
+        self.bridge = CvBridge()
+        params = cv2.SimpleBlobDetector_Params()
+        params.filterByArea = True
+        params.minArea = 1
+        print(params)
+        self.detector = cv2.SimpleBlobDetector_create()
 
     def _get_current_T(self, parent, child):
         """Get the most recent transformation matrix from child frame to parent frame."""
@@ -61,8 +72,37 @@ class Detector():
 
     def _rgb_callback(self, msg):
         try:
-            cv_rgb = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-            cv2.imwrite('rgb.png', cv_rgb)
+            # Convert ROS Image to an OpenCV image
+            cv_img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+            # Convert to HSV format.
+            cv_hsv = cv2.cvtColor(cv_img, cv2.COLOR_RGB2HSV)
+            
+            # Mask for blue and orange colors (based on HSV)
+            blue_masked = cv2.bitwise_and(cv_img, cv_img, mask=blue_mask(cv_hsv))
+            orange_masked = cv2.bitwise_and(cv_img, cv_img, mask=orange_mask(cv_hsv))
+
+            # Get grayscale versions of masked images (i.e. V channel in HSV)
+            blue_masked_gray = cv2.split(blue_masked)[-1]
+
+            # Compute connected components
+            # https://stackoverflow.com/questions/35854197/how-to-use-opencvs-connected-components-with-stats-in-python
+            thresh = cv2.threshold(blue_masked_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+            num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh, 8, cv2.CV_32S)
+            print("num_labels={}".format(num_labels))
+            # print("labels={}".format(labels))
+            # print("stats={}".format(stats))
+            # print("centroids={}".format(centroids))
+
+            # TODO:
+            # 1. Select white component and get centroid.
+            # 2. Synchronize with depth image using ApproximateTimeSynchronizer.
+            # 3. Find average depth of pixels in component.
+            # -- If not too dissimilar, ignore?
+            # 4. Assign the centroid at given depth as the location.
+            
+            # cv2.imwrite('blue.png', thresh)
+
+            
         except CvBridgeError as e:
             rospy.logerr(e)
     
